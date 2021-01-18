f4ca944073eb158b17c950792e957b49aa07dce3
YukimiSnow <happyvince999@gmail.com>
2021-01-18 21:43:02 +0800

    Yep
    Yes

---
+++ 06_Vince Lim Tze Yee_Lab 15/Assets/Scripts/BGM_Controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM_Controller : MonoBehaviour
+{
+
+    private AudioSource audioSource;
+    public AudioClip[] AudioClipBGM;
+    private float volumeChangeSpeed = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Selects a random BGM in the array
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            audioSource.Stop();
+            int rand = Random.Range(0, AudioClipBGM.Length);
+            audioSource.PlayOneShot(AudioClipBGM[rand]);
+        }
+        //Increase volume
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            audioSource.volume += Time.deltaTime * volumeChangeSpeed;
+        }
+        //Reduce volume
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            audioSource.volume -= Time.deltaTime * volumeChangeSpeed;
+        }
+    }
+
+}
